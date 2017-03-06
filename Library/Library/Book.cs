e692d990752fa55b9091cd8069c17bc4d5cfee09
3f14 <3f14@Liceospalla.local>
2017-03-06 11:25:00 +0100

    Aggiunta soluzione

---
+++ Library/Library/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        public string title { get; set; }
+
+        public string author { get; set; }
+
+        public int isbn { get; set; }
+
+        public enum genre {}
+    }
+}
