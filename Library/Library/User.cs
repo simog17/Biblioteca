0765cd6b3011643e578fe11aa36152931ba2dc35
simog17 <simogaddi17@gmail.com>
2017-03-07 16:46:36 +0100

    Aggiustamenti classe User

--- Library/Library/User.cs
+++ Library/Library/User.cs
@@ -15,24 +15,35 @@ namespace Library
         public string cf { get; set; }
 
         public DateTime dB { get; set; }
-        
 
-        public List <Book> ownBook = new List <Book> ();
+
+        public List<Book> ownBook = new List<Book>();
 
         public User(string name, string surname, string cf, int year, int month, int day)
         {
             DateTime dateBirth = new DateTime(year, month, day);
             dB = dateBirth;
+            this.name = name;
+            this.surname = surname;
+            this.cf = cf;
         }
 
         public override string ToString()
         {
-            return name + surname + dB;
+            string output = name + surname + dB;
+            return output;
         }
 
-        public string describe ()
+        public string describe()
         {
-            return "test";
+            string output = name + surname + cf + dB; // manca il numero dei libri
+            return output;
         }
+
+       /* public string describeBook()
+        {
+            string output = describe() + //libri che ha
+            return output;
+        }*/
     }
 }
