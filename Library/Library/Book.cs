a568d263af581a8e73d9d9f41df5e4d9540d7190
Ng2k <nicola.ng2k@gmail.com>
2017-03-07 20:10:15 +0100

    Aggiustamenti classe Book

--- Library/Library/Book.cs
+++ Library/Library/Book.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using System.Threading.Tasks;
 
 namespace Library
@@ -14,27 +15,49 @@ namespace Library
 
         public int isbn { get; set; }
 
-        public enum genre {Comedy, Horror, Fantasy, RomanceNovel}
+        private bool _booked;
+        public bool booked { get { return _booked; } }
 
-        public bool booked { get { return false; } }
+        public enum genres {Comedy, Horror, Fantasy, RomanceNovel}
+
+        public genres genre { get; set; } //proprietà di type genres
 
         //Costruttore
         public Book(string title, string author, int isbn, string genre)
         {
-
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+            switch (genre.ToLower())  // Assegnazione genere libro + normalizzazione dato
+            {
+                case "comedy":
+                    this.genre = genres.Comedy;
+                    break;
+                case "horror":
+                    this.genre = genres.Horror;
+                    break;
+                case "fantasy":
+                    this.genre = genres.Fantasy;
+                    break;
+                case "romancenovel":
+                    this.genre = genres.RomanceNovel;
+                    break;
+            }
         }
 
         public override string ToString()
         {
-            return isbn + " " + author;
+            return isbn + " " + author + "\r\n";
         }
 
-        public string Describe ()
+        public string Describe()
         {
-            return "Descrivo libro";
+            return title + " " + author + " " + genre + "\r\n";
         }
 
-        public bool IsBooked(User u)
+        // Se il libro è già stato prestato allora verrà impedito il prestito
+        // altrimenti esso verrà dato in prestito all'utente 
+        public bool ToBook(User u)
         {
             if (booked)
             {
@@ -42,6 +65,7 @@ namespace Library
             }
             else
             {
+                _booked = true;
                 return true;
             }
         }
